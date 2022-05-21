fb0cfe1680758e51ec3ea6594a2d8f5a398801c1
Jonas Souza <jonasmzsouza@gmail.com>
2022-05-21 15:19:02 -0300

    feat: add repositories

---
+++ backend/src/main/java/com/devsuperior/dsmovieflix/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dsmovieflix.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.dsmovieflix.entities.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
