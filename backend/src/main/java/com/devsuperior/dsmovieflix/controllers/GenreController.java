bfb8d5f06c0971ddfebf9d5fcea2ec4c13bd3428
Jonas Souza <jonasmzsouza@gmail.com>
2022-06-08 23:02:05 -0300

    feat(Genre): add findAll - dto, service, controller

---
+++ backend/src/main/java/com/devsuperior/dsmovieflix/controllers/GenreController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsmovieflix.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmovieflix.dto.GenreDTO;
+import com.devsuperior.dsmovieflix.services.GenreService;
+
+@RestController
+@RequestMapping(value = "/genres")
+public class GenreController {
+
+	@Autowired
+	private GenreService service;
+
+	@GetMapping
+	public ResponseEntity<List<GenreDTO>> findAll() {
+		List<GenreDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+}
--- backend/src/main/java/com/devsuperior/dsmovieflix/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovieflix/entities/Movie.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -26,6 +27,7 @@ public class Movie implements Serializable {
 	private String subTitle;
 	private Integer year;
 	private String imgUrl;
+	@Column(length = 1000)
 	private String synopsis;
 
 	@ManyToOne
