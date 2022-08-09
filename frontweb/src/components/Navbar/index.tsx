bb588b2d58d3f28f645a36ef784230cbbc3ebcc1
Jonas Souza <jonasmzsouza@gmail.com>
2022-08-08 22:16:26 -0300

    :heavy_plus_sign: update(frontweb): add navbar styling

---
+++ frontweb/src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
+          <h1>MovieFlix</h1>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
