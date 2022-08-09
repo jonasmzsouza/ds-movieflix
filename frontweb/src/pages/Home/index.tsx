a99083d0d18e46d6d23f38684fbdd025f74a42fb
Jonas Souza <jonasmzsouza@gmail.com>
2022-08-09 17:17:11 -0300

    :heavy_plus_sign: update(frontweb): add home styling

---
+++ frontweb/src/pages/Home/index.tsx
@@ -0,0 +1,46 @@
+import { ReactComponent as HomeImage } from 'assets/images/home-image.svg';
+import Button from 'components/Button';
+
+import './styles.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="home-banner-container">
+        <h2>Avalie Filmes</h2>
+        <p>
+          Diga o que você achou do seu<br></br> filme favorito
+        </p>
+        <HomeImage />
+      </div>
+      <div className="home-form-container">
+        <div className="base-card login-card">
+          <h1>LOGIN</h1>
+          <form>
+            <div className="mb-4">
+              <input
+                type="text"
+                className="form-control base-input"
+                placeholder="Email"
+                name="username"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                className="form-control base-input"
+                placeholder="Senha"
+                name="password"
+              />
+            </div>
+            <div className="login-submit">
+              <Button text="Fazer login" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
