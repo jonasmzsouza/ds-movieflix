81196c5ad2bf5bcaae2afb884da0c5045920c965
Jonas Souza <jonasmzsouza@gmail.com>
2022-08-13 18:20:12 -0300

    :heavy_plus_sign: update(frontweb): jwt-decode | check user authenticated | privateroute

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -1,4 +1,5 @@
 import Navbar from 'components/Navbar';
+import PrivateRoute from 'components/PrivateRoute';
 import Home from 'pages/Home';
 import MovieCatalog from 'pages/Private/MovieCatalog';
 import MovieDetails from 'pages/Private/MovieDetails';
@@ -13,12 +14,14 @@ const Routes = () => {
         <Route path={'/'} exact>
           <Home />
         </Route>
-        <Route path={'/movies'} exact>
-          <MovieCatalog />
-        </Route>
-        <Route path={'/movies/:movieId'}>
-          <MovieDetails />
-        </Route>
+        <PrivateRoute path={'/movies'}>
+          <Route path={'/movies'} exact>
+            <MovieCatalog />
+          </Route>
+          <Route path={'/movies/:movieId'}>
+            <MovieDetails />
+          </Route>
+        </PrivateRoute>
       </Switch>
     </Router>
   );
