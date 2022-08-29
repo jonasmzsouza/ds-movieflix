0b3e9fd53a58a44090c081b3cfba363fb7c73821
Jonas Souza <jonasmzsouza@gmail.com>
2022-08-29 20:14:34 -0300

    :heavy_plus_sign: update(frontweb): add moviecard styling

---
+++ frontweb/src/components/MovieCard/index.tsx
@@ -0,0 +1,23 @@
+import { Movie } from 'types/movie';
+import './styles.css';
+
+type Props = {
+  movie: Movie;
+};
+
+const MovieCard = ({ movie }: Props) => {
+  return (
+    <div className="base-card movie-card">
+      <div className="card-top-container">
+        <img src={movie.imgUrl} alt={movie.title} />
+      </div>
+      <div className="card-bottom-container">
+        <h5>{movie.title}</h5>
+        <strong>{movie.year}</strong>
+        <h6>{movie.subTitle}</h6>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- frontweb/src/pages/Private/MovieCatalog/index.tsx
+++ frontweb/src/pages/Private/MovieCatalog/index.tsx
@@ -1,13 +1,48 @@
+import MovieCard from 'components/MovieCard';
 import { Link } from 'react-router-dom';
 import './styles.css';
 
 const MovieCatalog = () => {
+  const movie = {
+    id: 1,
+    title: 'Bob Esponja',
+    subTitle: 'O Incrível Resgate',
+    year: 2020,
+    imgUrl:
+      'https://image.tmdb.org/t/p/w533_and_h300_bestv2/wu1uilmhM4TdluKi2ytfz8gidHf.jpg',
+  };
+
   return (
     <div className="catalog-container">
-      <h2>Tela de listagem de filmes</h2>
-      <div className="catalog-content-container">
-        <Link to={'/movies/1'}>Acessar /movies/1</Link>
-        <Link to={'/movies/2'}>Acessar /movies/2</Link>
+      <div className="row">
+        <div className="col-sm-6 col-xl-3 my-1" key={movie.id}>
+          <div className="catalog-content-container">
+            <Link to={`/movies/${movie.id}`}>
+              <MovieCard movie={movie} />
+            </Link>
+          </div>
+        </div>
+        <div className="col-sm-6 col-xl-3 my-1" key={movie.id}>
+          <div className="catalog-content-container">
+            <Link to={`/movies/${movie.id}`}>
+              <MovieCard movie={movie} />
+            </Link>
+          </div>
+        </div>
+        <div className="col-sm-6 col-xl-3 my-1" key={movie.id}>
+          <div className="catalog-content-container">
+            <Link to={`/movies/${movie.id}`}>
+              <MovieCard movie={movie} />
+            </Link>
+          </div>
+        </div>
+        <div className="col-sm-6 col-xl-3 my-1" key={movie.id}>
+          <div className="catalog-content-container">
+            <Link to={`/movies/${movie.id}`}>
+              <MovieCard movie={movie} />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
