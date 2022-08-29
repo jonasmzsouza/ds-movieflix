434f40b299961c4252521a3353607e79548cb597
Jonas Souza <jonasmzsouza@gmail.com>
2022-08-29 20:31:35 -0300

    :heavy_plus_sign: update(frontweb): add list catalog

--- frontweb/src/pages/Private/MovieCatalog/index.tsx
+++ frontweb/src/pages/Private/MovieCatalog/index.tsx
@@ -1,48 +1,49 @@
+import { AxiosRequestConfig } from 'axios';
 import MovieCard from 'components/MovieCard';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Movie } from 'types/movie';
+import { SpringPage } from 'types/vendor/spring';
+import { requestBackend } from 'utils/requests';
 import './styles.css';
 
 const MovieCatalog = () => {
-  const movie = {
-    id: 1,
-    title: 'Bob Esponja',
-    subTitle: 'O Incrível Resgate',
-    year: 2020,
-    imgUrl:
-      'https://image.tmdb.org/t/p/w533_and_h300_bestv2/wu1uilmhM4TdluKi2ytfz8gidHf.jpg',
+  const [page, setPage] = useState<SpringPage<Movie>>();
+
+  useEffect(() => {
+    getMovies(0);
+  }, []);
+
+  const getMovies = (pageNumber: number) => {
+    const params: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/movies',
+      params: {
+        page: pageNumber,
+        size: 4,
+      },
+      withCredentials: true,
+    };
+
+    requestBackend(params).then((response) => {
+      setPage(response.data);
+    });
   };
 
   return (
     <div className="catalog-container">
       <div className="row">
-        <div className="col-sm-6 col-xl-3 my-1" key={movie.id}>
-          <div className="catalog-content-container">
-            <Link to={`/movies/${movie.id}`}>
-              <MovieCard movie={movie} />
-            </Link>
-          </div>
-        </div>
-        <div className="col-sm-6 col-xl-3 my-1" key={movie.id}>
-          <div className="catalog-content-container">
-            <Link to={`/movies/${movie.id}`}>
-              <MovieCard movie={movie} />
-            </Link>
-          </div>
-        </div>
-        <div className="col-sm-6 col-xl-3 my-1" key={movie.id}>
-          <div className="catalog-content-container">
-            <Link to={`/movies/${movie.id}`}>
-              <MovieCard movie={movie} />
-            </Link>
-          </div>
-        </div>
-        <div className="col-sm-6 col-xl-3 my-1" key={movie.id}>
-          <div className="catalog-content-container">
-            <Link to={`/movies/${movie.id}`}>
-              <MovieCard movie={movie} />
-            </Link>
-          </div>
-        </div>
+        {page?.content.map((movie) => {
+          return (
+            <div className="col-sm-6 col-xl-3 my-1" key={movie.id}>
+              <div className="catalog-content-container">
+                <Link to={`/movies/${movie.id}`}>
+                  <MovieCard movie={movie} />
+                </Link>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
